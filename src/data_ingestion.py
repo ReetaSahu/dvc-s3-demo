11d0decc4b35bc353d44a46fa4b86e19517b3dcd
Reeta Sahu <sahureeta1997@gmail.com>
2024-07-06 01:16:19 +0530

    PC 3

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -4,7 +4,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 import os
 
-df = pd.read_csv('./data/raw/student_performance.csv')
+df = pd.read_csv('./data/raw/student_data.csv')
 
 # Separating features and target variable
 X = df.drop(columns=['Placement'])
@@ -15,11 +15,11 @@ scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
 # Applying PCA
-pca = PCA(n_components=2)
+pca = PCA(n_components=3)
 X_pca = pca.fit_transform(X_scaled)
 
 # Creating a DataFrame with PCA results
-df_pca = pd.DataFrame(data=X_pca, columns=['PC1', 'PC2'])
+df_pca = pd.DataFrame(data=X_pca, columns=['PC1', 'PC2','PCA3'])
 df_pca['Placement'] = y.values
 
 df_pca.to_csv(os.path.join('data','processed','student_performance_pca.csv'), index=False)
